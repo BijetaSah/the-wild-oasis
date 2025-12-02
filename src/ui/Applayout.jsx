4904a234b93b8ec87b2bd5b5061225096066b288
Bijeta Sah <bijetasah30@gmail.com>
2025-12-02 14:25:08 +0530

    feat : add a layout for the page

--- src/ui/Applayout.jsx
+++ src/ui/Applayout.jsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 24rem 1fr;
+  grid-template-rows: auto auto auto;
+`;
 
 function Applayout() {
   return (
-    <div>
-      applayout
-      <Outlet />
-    </div>
+    <StyledDiv>
+      <Header />
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
+    </StyledDiv>
   );
 }
 
