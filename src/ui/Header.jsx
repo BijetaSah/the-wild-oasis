4239e14d115efb8ff72337a3c6f7b9c81835580f
Bijeta Sah <bijetasah30@gmail.com>
2025-12-10 20:23:24 +0530

    feat : create a reusbale heading used as h1 h2 h3

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   background: var(--color-grey-50);
-  border-bottom: var(--color-grey-100);
-  /* padding: 4.8rem; */
-  height: 9rem;
+  border-bottom: 1px solid var(--color-grey-100);
+  padding: 1.2rem 4.8rem;
+  /* height: 9rem; */
 `;
 
 function Header() {
